a709c7817adc90c35b45fe5a1acef7df9d3d2bfb
bernardoabreu <bernardoaabreu@gmail.com>
2018-04-22 02:24:44 -0300

    Add node and point classes

--- tp1/node.py
+++ tp1/node.py
@@ -5,3 +5,10 @@ class Node():
         self.parent = parent
         self.action = action
         self.path_cost = path_cost
+
+    def __str__(self):
+        return '<' + str(self.state.x) + ',' + str(self.state.y) + ',' + \
+               str(self.path_cost) + '>'
+
+    def __repr__(self):
+        return self.__str__()
